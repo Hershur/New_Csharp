daa4f3733aef3cc5e64813c01247ce4d1c417232
Assurance Femi <assurancefemi@gmail.com>
2019-07-10 16:34:11 +0100

    new commit

--- Program.cs
+++ Program.cs
@@ -1,19 +1,69 @@
 ﻿using System;
 
-namespace DataTypes
+namespace lesson_08_enums_and_structs
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //constructor bears same name with he class and acts as a a method
-            Console.WriteLine("Enter your name");
-            var inputString = Console.ReadLine();
-            Console.WriteLine($"Hello {inputString}, what year were you born?");
-            var birthYear = int.Parse(Console.ReadLine());
-            int age = 2019 - birthYear;
-            Console.WriteLine($"Wow, you are {age} years old");
-            
+            //enums
+            Console.WriteLine("Kindly rate your banking experience where 1 is Very Dissatisfied and 5 is Very Satisfied");
+            var customerRatingInput=int.Parse(Console.ReadLine());
+            var customerRating=(Rating)customerRatingInput;
+            Console.WriteLine(customerRating);
+            Console.WriteLine("What is the weight of the child born?");
+            double newBornWeight=double.Parse(Console.ReadLine());
+            Console.WriteLine("What is the gender? 1 for male, 2 for female");
+            int newBornGender=int.Parse(Console.ReadLine());
+        
+
+            WeightClass weightClass;
+            if (newBornWeight<1.0){
+                weightClass=WeightClass.Underweight;
+            }
+            else if (newBornWeight>=1&&newBornWeight<=2.0) {
+                weightClass=WeightClass.Normal;
+            }
+            else {
+                weightClass=WeightClass.Overweight;
+            }
+
+            Child newBorn=new Child
+            {
+                Weight=newBornWeight,
+                Gender=(Gender)newBornGender,
+                WeightClass=weightClass
+            };
+            System.Console.WriteLine($"Your child is {weightClass}");
+            Console.WriteLine($"A {newBorn.Gender} child was born and weighs {newBorn.Weight}kg");
         }
     }
+
+    enum WeightClass {
+        Underweight,
+        Normal,
+        Overweight
+    }
+    struct Child
+    {
+        public double Weight;
+        public Gender Gender;
+        public WeightClass WeightClass;
+    }
+    enum Gender
+    {
+     Male=1,
+     Female
+    }
+   enum Rating
+    {
+     VeryDissatisfied=1,
+     Dissatisfied,
+     Indifferent,
+    Satisfied,
+    VerySatisfied
+    
+    }
+
+    
 }
