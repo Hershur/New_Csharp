7d0e5a0c27fff609e70c5ec68f4e830860d319b7
Assurance Femi <assurancefemi@gmail.com>
2019-07-10 12:17:42 +0100

    third commit

--- Program.cs
+++ Program.cs
@@ -7,8 +7,13 @@ namespace DataTypes
         static void Main(string[] args)
         {
             //constructor bears same name with he class and acts as a a method
-            int myInt = 24;
-            Console.WriteLine($"{myInt} is a datatype of {myInt.GetType()}");
+            Console.WriteLine("Enter your name");
+            var inputString = Console.ReadLine();
+            Console.WriteLine($"Hello {inputString}, what year were you born?");
+            var birthYear = int.Parse(Console.ReadLine());
+            int age = 2019 - birthYear;
+            Console.WriteLine($"Wow, you are {age} years old");
+            
         }
     }
 }
